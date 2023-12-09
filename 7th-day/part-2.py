f1a09ed022f5dde1dd60175e6cc9899264d9d3bb
S7iso <82435868+S7iso-Maseko@users.noreply.github.com>
2023-12-09 12:40:55 +0200

    Update part-2.py
    Changed a few variable names - added some comments

--- 7th-day/part-2.py
+++ 7th-day/part-2.py
@@ -7,21 +7,22 @@ with open(file_path, 'r') as file:
     for line in file: 
         hands.append(line.split())
 
+# Decided to map the cards to letters, that way I can sort them with using a Python sort function
 cards1 = ["A", "K", "Q", "T", "J", "9", "8", "7", "6", "5", "4", "3", "2"]
 cards2 = ["N", "M", "L", "K", "A", "I", "H", "G", "F", "E", "D", "C", "B"]
 
 wins = []
-handss = []
+updated_hands = []
 
 for hand, bid in hands:
     for i in range(len(cards1)):
         hand = hand.replace(str(cards1[i]), cards2[i])
-    handss.append((hand, bid))
+    updated_hands.append((hand, bid))
 
 cards2 = ["N", "M", "L", "K", "I", "H", "G", "F", "E", "D", "C", "B"]
 
 difset = []
-for hand, bid in handss:
+for hand, bid in updated_hands:
     sets = []
     for i in cards2:
         if hand.count(str(i)) > 1:
@@ -38,9 +39,7 @@ for hand, bid in handss:
                     sets.append(hand.count(difset[1]) + int(hand.count(str('A'))))
                     sets.append(hand.count(difset[0])) 
             else: sets.append(hand.count(difset[0]) + int(hand.count(str('A')))) 
-        elif int(hand.count(str('A'))) + 1 < 5:
-            sets.append(int(hand.count(str('A'))) + 1)
-        elif int(hand.count(str('A'))) + 1 == 5:
+        elif int(hand.count(str('A'))) + 1 <= 5:
             sets.append(int(hand.count(str('A'))) + 1)
         else: sets.append(int(hand.count(str('A'))))
     difset = []
