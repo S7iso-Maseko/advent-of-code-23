6f16e83d49208083ee439827acf9d8804ca4c325
NQ-edile <nqfreelance@gmail.com>
2024-08-25 20:30:32 +0200

    refreshing on data structures, cos why not

---
+++ assessment-prep/binary-tree/btree.py
@@ -0,0 +1,37 @@
+class bTree:
+    def __init__ (self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+    def insert(self, data):        
+        if self.data == data:
+            return
+        
+        if data < self.data:
+            if self.left:
+                self.left.insert(data)
+            else:
+                self.left = bTree(data)
+        
+        else:
+            if self.right:
+                self.right.insert(data)
+            else:
+                self.right = bTree(data)
+        
+    def inOrder(self, b): 
+        self.inOrder(b.left)
+        print(b.data)
+        self.inOrder(b.right)
+        
+
+b = bTree(16)
+b.insert(15)
+b.insert(4)
+b.insert(17)
+b.insert(3)
+b.insert(20)
+
+b.inOrder(b)
+        
